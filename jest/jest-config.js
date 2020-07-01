c18e0eb91ca980f61a48f184a3c89f833fe1fb5c
Adam Teuscher <adam.m.teuscher@gmail.com>
2020-06-30 23:49:53 -0400

    path issue was unrelated, found a workaround

--- jest/jest-config.js
+++ jest/jest-config.js
@@ -15,7 +15,8 @@ module.exports = {
     'node_modules/(?!(gatsby)/)'
   ],
   globals: {
-    __PATH_PREFIX__: ''
+    __PATH_PREFIX__: '',
+    __BASE_PATH__: ``, // add this line
   },
   setupFiles: ['<rootDir>/jest/loadershim.js']
 };
